344b9a8e6a2e7f3191f64dd18ad374e9340be785
JessQi <85958041+jessq1@users.noreply.github.com>
2021-11-30 16:32:36 -0500

    add signup form

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,28 +1,39 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 // import * as authService from '../../services/authService'
 import {Box,  Button, TextField } from '@mui/material';
 
-class SignupForm extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: '',
-    passwordConf: '',
+// interface IState {
+//     user: {
+//         name: string,
+//         email: string,
+//         password: string,
+//         passwordConf: string,
+//     }[]
+// }
+
+function SignupForm() {
+
+    const [input, setInput] = useState({
+            name: '',
+            email: '',
+            password: '',
+            passwordConf: '',
+    })
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement> ) => {
+//     props.updateMessage('')
+    setInput({
+        ...input,
+        [e.target.name]: e.target.value,
+    })
   }
 
-//   handleChange = (e: any) => {
-//     this.props.updateMessage('')
-//     this.setState({
-//       [e.target.name]: e.target.value,
-//     })
-//   }
-
 //   handleSubmit = async e => {
-//     const { history, updateMessage, handleSignupOrLogin } = this.props
+//     const { history, updateMessage, handleSignupOrLogin } = props
 //     e.preventDefault()
 //     try {
-//       await authService.signup(this.state)
+//       await authService.signup(state)
 //       handleSignupOrLogin()
 //       history.push('/')
 //     } catch (err) {
@@ -30,79 +41,75 @@ class SignupForm extends Component {
 //     }
 //   }
 
-  isFormInvalid() {
-    const { name, email, password, passwordConf } = this.state
+  const isFormInvalid = () => {
+    const { name, email, password, passwordConf } = input
     return !(name && email && password === passwordConf)
   }
 
-  render() {
-    const { name, email, password, passwordConf } = this.state
-    return (
-    //   <form
-    //     autoComplete="off"
-    //     onSubmit={this.handleSubmit}
-    //     className={styles.container}
-    //   >
-    //     <div className={styles.inputContainer}>
-    //       {/* <label htmlFor="name" className={styles.label}>
-    //         Name
-    //       </label> */}
-    //       <TextField
-    //         type="text"
-    //         autoComplete="off"
-    //         id="name"
-    //         value={name}
-    //         name="name"
-    //         onChange={this.handleChange}
-    //         label="Name"
-    //         variant="outlined"
-    //       />
-    //     </div>
-    //     <div className={styles.inputContainer}>
-    //       <TextField
-    //         type="text"
-    //         autoComplete="off"
-    //         id="email"
-    //         value={email}
-    //         name="email"
-    //         onChange={this.handleChange}
-    //         label="Email"
-    //         variant="outlined"
-    //       />
-    //     </div>
-    //     <div className={styles.inputContainer}>
-    //       <TextField
-    //         type="password"
-    //         autoComplete="off"
-    //         id="password"
-    //         value={password}
-    //         name="password"
-    //         onChange={this.handleChange}
-    //         label="Password"
-    //         variant="outlined"
-    //       />
-    //     </div>
-    //     <div className={styles.inputContainer}>
-    //       <TextField
-    //         type="password"
-    //         autoComplete="off"
-    //         id="confirm"
-    //         value={passwordConf}
-    //         name="passwordConf"
-    //         onChange={this.handleChange}
-    //         label="Confirm Password"
-    //         variant="outlined"
-    //       />
-    //     </div>
-    //     <div className={styles.inputContainer}>
-    //       <Button className={styles.Button} color={'primary'} variant="outlined" type='submit' disabled={this.isFormInvalid()} >Sign Up</Button>
-    //       <Button component={Link} to="/"color={'primary'}variant="outlined" >Cancel</Button>
 
-    //     </div>
-    //   </form>
-    <div>SignupForm</div>
+    const { name, email, password, passwordConf } = input
+    return (
+      <form
+        autoComplete="off"
+    //     onSubmit={handleSubmit}
+      >
+         <div >
+           <TextField
+             type="text"
+             autoComplete="off"
+             id="name"
+             value={name}
+             name="name"
+             onChange={handleChange}
+             label="Name"
+             variant="outlined"
+           />
+         </div>
+         <div >
+           <TextField
+             type="text"
+             autoComplete="off"
+             id="email"
+             value={email}
+             name="email"
+             onChange={handleChange}
+             label="Email"
+             variant="outlined"
+           />
+         </div>
+         <div >
+           <TextField
+             type="password"
+             autoComplete="off"
+             id="password"
+             value={password}
+             name="password"
+             onChange={handleChange}
+             label="Password"
+             variant="outlined"
+           />
+         </div>
+         <div >
+           <TextField
+             type="password"
+             autoComplete="off"
+             id="confirm"
+             value={passwordConf}
+             name="passwordConf"
+             onChange={handleChange}
+             label="Confirm Password"
+             variant="outlined"
+           />
+         </div>
+         <div >
+           <Button  color={'primary'} variant="outlined" type='submit' disabled={isFormInvalid()} >Sign Up</Button>
+           <Button component={Link} to="/"color={'primary'}variant="outlined" >Cancel</Button>
+
+         </div>
+      </form>
+    // <div>SignupForm</div>
     )
-  }
+
 }
 
 export default SignupForm
