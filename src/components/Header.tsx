ee4a39b58556d1df9332538ce108a02cf913cc75
JessQi <85958041+jessq1@users.noreply.github.com>
2021-11-30 11:24:06 -0500

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+interface HeaderProps {
+  title: string;
+}
+
+export default function Header(props: HeaderProps) {
+  const { title } = props;
+
+  return (
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Button size="small">Subscribe</Button>
+        <Typography
+          component="h2"
+          variant="h4"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          {title}
+        </Typography>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <Button variant="outlined" size="small">
+          Sign up
+        </Button>
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+      >
+        
+      </Toolbar>
+    </React.Fragment>
+  );
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header title = 'poly'/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
