7a4e2c27f2da1476ddff1624c5ba09e994402211
JessQi <85958041+jessq1@users.noreply.github.com>
2021-11-30 13:01:09 -0500

    adjust nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 
 interface HeaderProps {
   title: string;
@@ -16,20 +13,17 @@ export default function Header(props: HeaderProps) {
   return (
     <React.Fragment>
       <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Button size="small">Subscribe</Button>
         <Typography
           component="h2"
           variant="h4"
           color="inherit"
           align="center"
-          noWrap
+          noWrap 
           sx={{ flex: 1 }}
         >
-          {title}
+            {title}
         </Typography>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
+        
         <Button variant="outlined" size="small">
           Sign up
         </Button>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -9,20 +9,8 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
         <Header title = 'poly'/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
     </ThemeProvider>
   );
