81df0465135af4213963ccbdef9f6fc899167317
JessQi <85958041+jessq1@users.noreply.github.com>
2021-12-01 20:45:41 -0500

    add landing

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import React, {Component, useState} from 'react';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import { createMemoryHistory } from 'history';
 
 // connection to backend
@@ -10,6 +10,7 @@ import * as profileAPI from '../services/profileService'
 import Header from '../components/Header';
 import Signup from './Signup/Signup';
 import Login from './Login/Login';
+import Landing from './Landing/Landing'
 
 // styles:
 import {ThemeProvider} from '@mui/material/styles';
@@ -18,10 +19,17 @@ import { theme } from '../styles/theme';
 
 function App() {
   let history = createMemoryHistory();
+  const navigate = useNavigate()
   const [info, setInfo] = useState({
     user: authService.getUser(),
 		userProfile: null,
 })
+
+  const handleLogout = () => {
+    authService.logout()
+    setInfo({user:null, userProfile: null,})
+    navigate('/')
+  }
 
   const handleSignupOrLogin = async () => {
 		setInfo({ user: authService.getUser(), userProfile: await profileAPI.getUserProfile() })
@@ -30,9 +38,10 @@ function App() {
     <ThemeProvider theme={theme}>
         <Header title = 'poly'/>
         <Routes>
+        
+          <Route path='/' element={<Landing user={info.user} />} />
           <Route path='/signup' element={<Signup history={history} handleSignupOrLogin={handleSignupOrLogin} />} />
           <Route path='/login' element={<Login history={history} handleSignupOrLogin={handleSignupOrLogin} />} />
-          <Route path='/landing' />
         </Routes>
     </ThemeProvider>
   );
---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+interface IProps {
+  user: any,
+}
+
+const Landing: React.FC<IProps> = ({user}) => {
+  
+      return (
+        <>
+          
+          <div className='player-wrapper'>
+          <p>hello</p>
+          </div>
+  
+        </>
+      )
+  };
+   
+export default Landing
